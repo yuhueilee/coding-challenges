97c8b287e5eee7af9fc5ca4a33658558ced6af9e
yuhueilee <ylee0026@student.monash.edu>
2022-03-10 19:55:57 +0800

    Add comments

--- findRangeOfTargetInLst.py
+++ findRangeOfTargetInLst.py
@@ -12,6 +12,22 @@ from typing import List
 
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        """Find the start and end index of the target in nums (sorted, ascending) array.
+
+        Args:
+            nums (List[int]): a list of integers (incl. positive and negative)
+            target (int): an integer
+
+        Returns:
+            List[int]: start and end index
+        
+        Time complexity:
+            Best O(logN)
+            Worst O(logN)
+            where N is the number of elements in the nums array
+
+        Space complexity:
+        """
         start, end = -1, -1
         # find the first index of target in nums array
         index = self.binarySearch(nums, target, 0, len(nums) - 1)
@@ -40,10 +56,13 @@ class Solution:
         if left <= right:
             mid = (left + right) // 2
             if target == nums[mid]:
+                # terminate recursion when reaching the mid pointer reaches the edge of array
                 if mid == left:
                     return mid
                 else:
+                    # memorize the last found target index
                     lastest_index = mid
+                    # continue recursion to the left side to ensure no other target exists
                     return self.findStart(nums, target, left, mid - 1, lastest_index)
             elif target > nums[mid]:
                 left = mid + 1
@@ -57,10 +76,13 @@ class Solution:
         if left <= right:
             mid = (left + right) // 2
             if target == nums[mid]:
+                # terminate recursion when reaching the mid pointer reaches the edge of array
                 if mid == right:
                     return mid
                 else:
+                    # memorize the last found target index
                     lastest_index = mid
+                    # continue recursion to the right side to ensure no other target exists
                     return self.findEnd(nums, target, mid + 1, right, lastest_index)
             elif target > nums[mid]:
                 left = mid + 1
